df260025805075bfac486acf702c4d95c73351cc
kvadmin <klemen.vicic@unicreditgroup.si>
2019-03-04 12:56:31 +0100

    initial commit

---
+++ build/precache-manifest.8a556ff3a9cdd2d040812cfa1b9d1f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "41c9cd68f0caa7590eee",
+    "url": "/static/js/main.41c9cd68.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "41c9cd68f0caa7590eee",
+    "url": "/static/css/main.3f6fe9d8.chunk.css"
+  },
+  {
+    "revision": "3d26ff52985aaf09050d4600091e8a26",
+    "url": "/index.html"
+  }
+];
